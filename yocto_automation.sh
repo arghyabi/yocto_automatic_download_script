92f4fbe9d4f38c44e04e7d2d3f1505fb96b070f1
Arghya Biswas <arghyabiswas05@gmail.com>
2020-04-11 21:06:24 +0530

    Add files via upload

---
+++ yocto_automation.sh
@@ -0,0 +1,11 @@
+mkdir -p Yocto/
+mkdir -p Yocto/RaspberryPi/
+mkdir -p Yocto/RaspberryPi/layers
+
+cd Yocto/RaspberryPi/layers
+git clone -b warrior git://git.yoctoproject.org/poky.git
+git clone -b warrior git://git.openembedded.org/meta-openembedded
+git clone -b warrior git://git.yoctoproject.org/meta-raspberrypi
+git clone -b warrior git://github.com/meta-qt5/meta-qt5.git
+
+git clone git@github.com:arghyaBiswas05/meta-iAloy.git
